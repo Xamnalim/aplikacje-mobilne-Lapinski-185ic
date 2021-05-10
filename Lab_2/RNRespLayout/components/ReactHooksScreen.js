381e723c3bbdb148feaa2005ad40f71f33869899
Maksymilian Łapiński <mlapinski98@gmail.com>
2021-05-10 16:19:17 +0200

    Dodano 3 ekrany

---
+++ Lab_2/RNRespLayout/components/ReactHooksScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ReactHooksScreen() {
+  return (
+    <View>
+      <Text>Hello, React Hooks!</Text>
+    </View>
+  );
+}
