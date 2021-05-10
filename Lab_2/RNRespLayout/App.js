c28965c79bf9f2cdc0ea632cbee007fbc856cb6f
Maksymilian Łapiński <mlapinski98@gmail.com>
2021-05-10 16:19:48 +0200

    Dodano nawigacje

--- Lab_2/RNRespLayout/App.js
+++ Lab_2/RNRespLayout/App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Icons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import SpreadOperScreen from "./components/SpreadOperScreen";
+import RestParamsScreen from "./components/RestParamsScreen";
+import ReactHooksScreen from "./components/ReactHooksScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Spread Operator">
+        <Tab.Screen
+          name="Spread Operator"
+          component={SpreadOperScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return (
+                <Icons name={"dots-horizontal"} size={size} color={color} />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Rest Parameters"
+          component={RestParamsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Icons name={"sleep"} size={size} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="React Hooks useState"
+          component={ReactHooksScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Icons name={"hook"} size={size} color={color} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
